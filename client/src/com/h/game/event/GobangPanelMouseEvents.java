48fcad5c5a0a18041be1ad5e49aad8094629ca0c
2606710413@qq.com <2606710413@qq.com>
2022-06-17 10:50:46 +0800

    fix

--- client/src/com/h/game/event/GobangPanelMouseEvents.java
+++ client/src/com/h/game/event/GobangPanelMouseEvents.java
@@ -4,7 +4,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 
-public class GobangPanelMouseEvents extends MouseAdapter implements MouseMotionListener {
+public class GobangPanelMouseEvents extends MouseAdapter {
     private final GobangPanelMoushCallback gobangPanelMoushCallback;
 
     public GobangPanelMouseEvents(GobangPanelMoushCallback gobangPanelMoushCallback) {

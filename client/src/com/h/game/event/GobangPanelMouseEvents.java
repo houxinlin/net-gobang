e27ba0292b442926fd0b4efda78ca828ba69e440
2606710413@qq.com <2606710413@qq.com>
2022-06-17 10:50:08 +0800

    fix

--- client/src/com/h/game/event/GobangPanelMouseEvents.java
+++ client/src/com/h/game/event/GobangPanelMouseEvents.java
@@ -1,10 +1,10 @@
 package com.h.game.event;
 
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
-public class GobangPanelMouseEvents implements MouseListener, MouseMotionListener {
+public class GobangPanelMouseEvents extends MouseAdapter implements MouseMotionListener {
     private final GobangPanelMoushCallback gobangPanelMoushCallback;
 
     public GobangPanelMouseEvents(GobangPanelMoushCallback gobangPanelMoushCallback) {
@@ -12,33 +12,8 @@ public class GobangPanelMouseEvents implements MouseListener, MouseMotionListene
     }
 
     @Override
-    public void mouseClicked(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-
-    }
-
-    @Override
     public void mouseReleased(MouseEvent e) {
         if (gobangPanelMoushCallback != null) gobangPanelMoushCallback.mouseReleased(e);
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent e) {
-
     }
 
     @Override
